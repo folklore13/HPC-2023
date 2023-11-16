cf595a06c4cb0b44519b8cd8c7f8418f6c2fe5b0
Ario Mochammad Azizhi <ario.hitmeup@gmail.com>
2023-11-16 10:22:11 +0700

    Created the CRUD logic in BookController

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,8 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book;
 
 class BookController extends Controller
 {
-    //
+    public function index()
+    {
+        $books = Book::all();
+        return response()->json($books);
+    }
+
+    public function store()
+    {
+        $books = Book::create(request()->all());
+        return response()->json($books);
+    }
+
+    public function show(Book $book)
+    {
+        return response()->json($book);
+    }
+
+    public function update(Book $book)
+    {
+        $book->update(request()->all());
+        return response()->json($book);
+    }
+
+    public function destroy(Book $book)
+    {
+        $book->delete();
+        return response()->json(["message" => "Successfully deleted book"], 204);
+    }
 }
